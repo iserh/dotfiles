f6c66ae8e229c3c20153563c3a69b7126fa7473c
Henri Iser <henri.iser@iais.fraunhofer.de>
2023-05-19 16:26:43 +0200

    New python setup.

--- python/scripts/format.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-isort $(find . -type f -name '*.py')
-python -m reindent -r -n $(find . -type f -name '*.py')
-black .
--- python/scripts/lint.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env bash
-flake8 $(find . -type f -name '*.py')
