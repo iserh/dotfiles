a054415fefb511f05a2448f89d856a439a490648
Henri Iser <henri.iser@t-systems.com>
2026-09-08 18:02:34 +0200

    chezmoi: install tmux plugins against a server that has read the config

--- .chezmoiscripts/run_once_after_40-tpm.sh
+++ .chezmoiscripts/run_once_after_40-tpm.sh
@@ -5,9 +5,15 @@ TPM_DIR="$HOME/.tmux/plugins/tpm"
 
 if [ -d "$TPM_DIR" ]; then
     echo "==> tpm already installed"
-    exit 0
+else
+    echo "==> Installing tpm"
+    git clone --depth=1 https://github.com/tmux-plugins/tpm "$TPM_DIR"
 fi
 
-echo "==> Installing tpm"
-git clone --depth=1 https://github.com/tmux-plugins/tpm "$TPM_DIR"
+# install_plugins talks to a running server, and only a server that has read
+# ~/.tmux.conf knows about tpm at all.
+echo "==> Installing tmux plugins"
+session=$(tmux new-session -dP -F '#{session_id}')
+tmux source-file "$HOME/.tmux.conf"
 "$TPM_DIR/bin/install_plugins"
+tmux kill-session -t "$session"
